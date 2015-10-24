5fd67c9a39653b85e36ffdbf8c9cf7b0fc8b4d0d
mr-fool <mr-fool@localhost.localdomain>
2015-10-23 23:24:39 -0600

    cipher issue

---
+++ Cipher/app/src/main/java/mr_fool/cipher/DecryptMenu.java
@@ -0,0 +1,13 @@
+package mr_fool.cipher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DecryptMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_decrypt_menu);
+    }
+}
