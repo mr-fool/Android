f0517ba2199ba6e6a5121064f0fbc551e3bafd8e
mr-fool <mr-fool@users.noreply.github.com>
2015-10-30 10:21:41 -0600

    Update Encrypt.java

--- Cipher/app/src/main/java/mr_fool/cipher/Encrypt.java
+++ Cipher/app/src/main/java/mr_fool/cipher/Encrypt.java
@@ -11,18 +11,21 @@ public class Encrypt extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_encrypt);
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
         String transition_key = extras.getString("SHIFT_KEY");
         int key = Integer.parseInt(transition_key);
         String content = extras.getString("MESSAGE");
 
+        //Testing receiving information correctly
+        // Create the text view
+        String message = "The shift key is " + transition_key + "and the content is " + content;
         TextView textView = (TextView)findViewById(R.id.displayText);
         textView.setTextSize(30);
         textView.setTextColor(Color.parseColor("#FFFFFF"));
         textView.setText(message);
         // Set the text view as the activity layout
-        setContentView(textView);
 
     }
 }
