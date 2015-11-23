43389709de2cd2d4463b34649664d7e09cc8ebd2
mr-fool <mr.law.jason@gmail.com>
2015-11-22 19:59:24 -0700

    added extra file

---
+++ Terminal/app/src/main/java/mr_fool/terminal/ShellExecuter.java
@@ -0,0 +1,33 @@
+package mr_fool.terminal;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+/**
+ * Created by mr-fool on 22/11/15.
+ */
+public class ShellExecuter {
+    public ShellExecuter() {
+
+    }
+    public String Executer(String command) {
+
+        StringBuffer output = new StringBuffer();
+
+        Process p;
+        try {
+            p = Runtime.getRuntime().exec(command);
+            p.waitFor();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+            String line = "";
+            while ((line = reader.readLine())!= null) {
+                output.append(line + "\n");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        String response = output.toString();
+        return response;
+
+    }
+}
